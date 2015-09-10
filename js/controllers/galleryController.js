d3d679645614c488a5dfc2d43f397d6f4ce150f3
vancore295 <silverfire295@gmail.com>
2015-09-10 09:30:59 -0400

    added new images to hait folder

--- js/controllers/galleryController.js
+++
@@ -1,5 +0,0 @@
-app.controller('MainController',['$scope',function($scope){
-	$scope.w = window.innerWidth;
-	$scope.h = window.innerHeight;
-	$scope.folder = '../img/haiti';
-}]);
--- js/galleryApp.js
+++ js/galleryApp.js
@@ -1,1 +1,15 @@
-var app = angular.module("gallery",[]);
+var app = angular.module("gallery", []);
+app.controller('GalleryController', ['$scope', function ($scope) {
+  $scope.folder = '../img/haiti/';
+
+}]);
+app.directive('carousel', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '=',
+
+    },
+    templateUrl: 'js/directives/carousel.html'
+  };
+});
