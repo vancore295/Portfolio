82a29f3a6faed338e7f9eba8a43b8098b55f0faf
vancore295 <silverfire295@gmail.com>
2017-05-21 10:33:51 -0400

    added reset buttons for madlibs

--- js/controllers/madlibsController.js
+++ js/controllers/madlibsController.js
@@ -1,23 +1,10 @@
-angular.module('portfolioApp').controller('madlibsController', ['$scope', function ($scope) {
-  $scope.title = "Madlibs";
+angular.module('portfolioApp').controller('madlibsController', ['$scope', function($scope) {
+    $scope.title = "Madlibs";
 
-  $scope.master = {};
-
-  $scope.update = function (greet) {
-    $scope.angular.master.cope(greet);
-  };
-
-  $scope.update = function (war) {
-    $scope.angular.master.cope(war);
-  };
-
-  $scope.update = function (proverb) {
-    $scope.angular.master.cope(proverb);
-  };
-
-  $scope.reset = function () {
-    $scope.user = angular.copy($scope.master);
-  };
-
-  $scope.reset();
+    $scope.ResetGame = function(words) {
+        for (var word in words) {
+            words[word] = '';
+        }
+        return words;
+    }
 }]);
