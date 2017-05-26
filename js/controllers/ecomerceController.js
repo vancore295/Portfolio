13b48da2c932740e6b8ef4ad83162a6730f00802
vancore295 <silverfire295@gmail.com>
2017-05-26 19:09:48 -0400

    added new data for shop

--- js/controllers/ecomerceController.js
+++ js/controllers/ecomerceController.js
@@ -34,27 +34,27 @@ angular.module('portfolioApp').controller('eSchopController', ['$scope', functio
     ];
 
     $scope.shirts = [{
-            title: "Gray T-Shirt",
-            imageMain: 'http://www.haneslocator.com/wp-content/uploads/2013/12/Hanes_CB_ecoSmart_5170_SmokeGray_Front1.jpg',
-            imageAlt: 'http://www.haneslocator.com/wp-content/uploads/2013/11/Hanes_nanoT_4980_SmokeGray_Back1.jpg',
+            title: "Cat Love",
+            imageMain: 'img/shop/shirts/cat1.jpg',
+            imageAlt: 'img/shop/shirts/cat2.jpg',
             price: 10
         },
         {
-            title: "Black T-Shirt",
-            imageMain: 'http://www.sunspel.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/4/0/4001_102.jpg',
-            imageAlt: 'http://cdn.shopify.com/s/files/1/0671/4319/products/jumpbox-fitness-mens-tshirt_back.jpg?v=1415306217',
+            title: "9 and 3/4",
+            imageMain: 'img/shop/shirts/934.jpg',
+            imageAlt: '',
             price: 10
         },
         {
-            title: "Blue T-Shirt",
-            imageMain: 'http://www.vulcan100.com/image/large/iPZ8kLwDT=sHTvV8m4VsjqcC75Yh/images/2012/07/29/article/kt10/kt10b00218/selected-drill-t-shirt-neon-blue-1550-zoom-0.jpg',
-            imageAlt: 'http://moritzaustralia.com.au/wp-content/uploads/2013/04/Moritz-tshirt-royal-blue-back.jpg',
+            title: "Geometric",
+            imageMain: 'img/shop/shirts/shapes.jpg',
+            imageAlt: '',
             price: 10
         },
         {
-            title: "Red T-Shirt",
-            imageMain: 'img/cloths/shirts/BlueFront.jpg',
-            imageAlt: 'img/cloths/shirts/Blueback.jpg',
+            title: "AIA",
+            imageMain: 'img/shop/shirts/AIA.png',
+            imageAlt: '',
             price: 10
         }
 
